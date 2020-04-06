954acd5e5c39b4ba366bcb1394ae1ce3299882f4
agohorel <3317203+agohorel@users.noreply.github.com>
2020-04-06 12:56:21 -0400

    lowercase env

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
 const server = require("./server.js");
-const port = process.ENV.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`server listening on port ${port}`));
